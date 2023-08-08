745079993fae2f74b5c6afd99faff87f3ee10e52
GI_WON <rldnjsqhdl@naver.com>
2023-08-08 16:37:45 +0900

    기원

--- src/main/java/com/pro/infomate/addressbook/repository/ContactRepository.java
+++ src/main/java/com/pro/infomate/addressbook/repository/ContactRepository.java
@@ -4,4 +4,5 @@ import com.pro.infomate.addressbook.entity.Contact;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ContactRepository extends JpaRepository<Contact, Integer> {
+
 }
