c6451634f203c019e7b37767369e92d8e0f1f94b
j2seon <ddj04323@gmail.com>
2023-08-14 08:45:20 +0900

    레포지토리생성

---
+++ src/main/java/com/pro/infomate/approval/repository/ApprovalRepository.java
@@ -0,0 +1,10 @@
+package com.pro.infomate.approval.repository;
+
+import com.pro.infomate.approval.entity.Approval;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ApprovalRepository extends JpaRepository<Approval, Long> {
+
+
+
+}
