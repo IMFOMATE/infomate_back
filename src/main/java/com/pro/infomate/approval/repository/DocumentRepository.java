61df18abec137fc7a0dc39462bc2902d355fca77
j2seon <ddj04323@gmail.com>
2023-08-14 08:45:20 +0900

    레포지토리생성

--- src/main/java/com/pro/infomate/approval/repository/DocumentRepository.java
+++ src/main/java/com/pro/infomate/approval/repository/DocumentRepository.java
@@ -12,4 +12,11 @@ public interface DocumentRepository<T extends Document> extends JpaRepository<T,
 
 //    List<Document> findByDocumentKindEquals(String kind);
 
+  //1. 결제할 문서(3), 작성한문서(5), 참조문서(5) 개씩 보여주기
+  
+  //2. 작성한 문서 최신순 또는 결제 상태별로 조회 
+  
+  //3. 임시저장상태의 문서 최신순으로 조회
+
+
 }
--- src/main/java/com/pro/infomate/config/JpaConfig.java
+++ src/main/java/com/pro/infomate/config/JpaConfig.java
@@ -1,11 +1,24 @@
 package com.pro.infomate.config;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import javax.persistence.EntityManager;
 
 @Configuration
 @EntityScan(basePackages = "com.pro.infomate")
 @EnableJpaRepositories(basePackages = "com.pro.infomate")
+@RequiredArgsConstructor
 public class JpaConfig {
+
+  private final EntityManager em;
+  @Bean
+  public JPAQueryFactory queryFactory(){
+    return new JPAQueryFactory(em);
+
+  }
 }
