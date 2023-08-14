93b95760fe17567201f52f59de24bf51a7614b9c
GI_WON <rldnjsqhdl@naver.com>
2023-08-14 10:35:55 +0900

    기원 오류수정

--- src/main/java/com/pro/infomate/email/repository/TrashRepository.java
+++ src/main/java/com/pro/infomate/email/repository/TrashRepository.java
@@ -4,4 +4,5 @@ import com.pro.infomate.email.entity.Trash;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TrashRepository extends JpaRepository<Trash, Integer> {
+
 }
