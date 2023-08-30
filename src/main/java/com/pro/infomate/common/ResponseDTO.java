73c4825d312baa89d0661b3598e2682ccd270647
duckdin_ <dbrs96@gmail.com>
2023-08-30 14:10:30 +0900

    회원가입 로그인2

--- src/main/java/com/pro/infomate/common/ResponseDTO.java
+++ src/main/java/com/pro/infomate/common/ResponseDTO.java
@@ -3,11 +3,13 @@ package com.pro.infomate.common;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
 import org.springframework.http.HttpStatus;
 
 
 @Getter
+@Setter
 @ToString
 public class ResponseDTO {
 
@@ -16,6 +18,9 @@ public class ResponseDTO {
     private String message;
 
     private Object data;
+
+    public ResponseDTO() {
+    }
 
     @Builder
     public ResponseDTO(HttpStatus status, String message, Object data) {
