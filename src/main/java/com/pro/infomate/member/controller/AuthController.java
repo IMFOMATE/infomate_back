dbf736e813ff16e32b2fa978f490bd484de82209
duckdin_ <dbrs96@gmail.com>
2023-09-06 15:19:49 +0900

    토큰에 memberCode, deptCode 추가

--- src/main/java/com/pro/infomate/member/controller/AuthController.java
+++ src/main/java/com/pro/infomate/member/controller/AuthController.java
@@ -3,12 +3,11 @@ package com.pro.infomate.member.controller;
 import com.pro.infomate.common.ResponseDTO;
 import com.pro.infomate.member.dto.MemberDTO;
 import com.pro.infomate.member.service.AuthService;
+import com.pro.infomate.member.service.RegistService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/auth")
@@ -16,8 +15,11 @@ public class AuthController {
 
     private final AuthService authService;
 
-    public AuthController(AuthService authService) {
+    private final RegistService registService;
+
+    public AuthController(AuthService authService, RegistService registService) {
         this.authService = authService;
+        this.registService = registService;
     }
 
     @PostMapping("/login")
@@ -33,5 +35,13 @@ public class AuthController {
         return ResponseEntity
                 .ok()
                 .body(new ResponseDTO(HttpStatus.CREATED, "회원가입 성공", authService.signup(memberDTO)));
+    }
+
+    @PostMapping("/regist")
+    public ResponseEntity<ResponseDTO> registMember(@ModelAttribute MemberDTO memberDTO, MultipartFile memberImage){
+
+        return ResponseEntity.ok()
+                .body(new ResponseDTO(HttpStatus.OK, "회원 등록 성공"
+                        , registService.registMember(memberDTO, memberImage)));
     }
 }
