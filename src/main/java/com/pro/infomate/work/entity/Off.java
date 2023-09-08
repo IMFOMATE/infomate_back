59d7966e996d226935b9a6df1171836f6da98ff6
j2seon <ddj04323@gmail.com>
2023-09-08 14:41:23 +0900

    연차 api 추가 및 수정중

--- src/main/java/com/pro/infomate/work/entity/Off.java
+++ src/main/java/com/pro/infomate/work/entity/Off.java
@@ -5,26 +5,36 @@ import lombok.*;
 
 import javax.persistence.*;
 import java.sql.Timestamp;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
-@ToString
 @Entity
 @Table(name = "TBL_OFF")
+@SequenceGenerator(
+        name = "OFF_CODE_GENERATOR",
+        sequenceName = "SEQ_TBL_OFF_OFF_CODE",
+        initialValue = 1,
+        allocationSize = 1
+)
 public class Off {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "OFF_CODE_GENERATOR")
     @Column(name = "OFF_CODE")
-    private int offCode;
+    private Integer offCode;
 
     @Column(name = "OFF_TYPE")
     private String offType;
 
-    @Column(name = "OFF_DATE")
-    private Timestamp offDate;
+    @Column(name = "OFF_START")
+    private LocalDateTime off_start;
+
+    @Column(name = "OFF_END")
+    private LocalDateTime off_end;
 
     @Column(name = "OFF_DAY")
     private int offDay;
@@ -32,11 +42,18 @@ public class Off {
     @Column(name = "OFF_REASON")
     private String offReason;
 
-//    @Column(name = "MEMBER_CODE")
-//    private int memberCode;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "MEMBER_CODE")
     private Member member;
 
+    @Builder
+    public Off(String offType, LocalDateTime off_start, LocalDateTime off_end, int offDay, String offReason, Member member) {
+        this.offType = offType;
+        this.off_start = off_start;
+        this.off_end = off_end;
+        this.offDay = offDay;
+        this.offReason = offReason;
+        this.member = member;
+    }
 }
---
+++ src/main/java/com/pro/infomate/work/service/WorkService.java
@@ -0,0 +1,79 @@
+package com.pro.infomate.work.service;
+
+import com.pro.infomate.exception.CannotFinishWorkException;
+import com.pro.infomate.exception.NotFindDataException;
+import com.pro.infomate.exception.WorkAlreadyExistsException;
+import com.pro.infomate.member.entity.Member;
+import com.pro.infomate.member.repository.MemberRepository;
+import com.pro.infomate.work.entity.Off;
+import com.pro.infomate.work.entity.Work;
+import com.pro.infomate.work.entity.WorkStatus;
+import com.pro.infomate.work.repository.OffRepository;
+import com.pro.infomate.work.repository.WorkRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class WorkService {
+
+  private final MemberRepository memberRepository;
+  private final WorkRepository workRepository;
+  private final OffRepository offRepository;
+
+  //1. 출근
+  // 출근 버튼누르면 멤버코드로 work에 insert
+  // 만약 오늘 날짜의 출근이 이미 됐으면 이미출근했다는 오류발생
+  public void attend(Integer memberCode){
+    Member member = memberRepository.findById(memberCode).orElseThrow(() -> new NotFindDataException("회원정보가 없습니다"));
+
+    Optional<Work> existing = workRepository.findByYearMonth(LocalDate.now());
+
+    if (existing.isPresent())
+      throw new WorkAlreadyExistsException("이번 달에 이미 작업이 존재합니다.");
+
+    Work work = Work.builder()
+            .timeStart(LocalDateTime.now())
+            .yearMonth(LocalDate.now())
+            .workStatus(WorkStatus.PROGRESS)
+            .member(member)
+            .build();
+
+    workRepository.save(work);
+  }
+
+  //2. 퇴근
+  //퇴근버튼 누르면 9시간 이하일 경우 퇴근할 수 없음
+  // 9시간 이상이면 퇴근 컬럼 update하고, 근무상태를 done
+  //휴가 신청이 완료되면 해당날짜에 approval에서 insert해주자
+
+  public void finish(Integer memberCode) {
+    Member member = memberRepository.findById(memberCode).orElseThrow(() -> new NotFindDataException("회원정보가 없습니다"));
+
+    Work work = workRepository.findByYearMonth(LocalDate.now()).orElseThrow(() -> new NotFindDataException("출근정보가 없습니다"));
+
+    LocalDateTime nowTime = LocalDateTime.now();
+    LocalDateTime attendTime = work.getTimeStart();
+
+    // 근무 시간 계산
+    Duration workDuration = Duration.between(attendTime, nowTime);
+
+    // 9시간 미만 근무일 경우 퇴근할 수 없음
+    if (workDuration.toHours() < 9) {
+      throw new CannotFinishWorkException("근무 시간이 9시간 미만입니다. 퇴근할 수 없습니다.");
+    }
+
+    // 근무 상태를 'done'으로 변경하고 퇴근 시간 설정
+    work.setWorkStatus(WorkStatus.DONE);
+    work.setTimeEnd(nowTime);
+
+  }
+
+  //만약 반차가 있을 경우 해당 날짜의 반차 니까 근무시간을 -4
+  //상태를 반차
+}
