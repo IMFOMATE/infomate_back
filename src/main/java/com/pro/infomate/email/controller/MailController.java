fd7cbd74744a7a3638244374b37da101449fa3b6
j2seon <ddj04323@gmail.com>
2023-09-12 14:32:36 +0900

    오류부분 수정해서 커밋

--- src/main/java/com/pro/infomate/email/controller/MailController.java
+++ src/main/java/com/pro/infomate/email/controller/MailController.java
@@ -21,7 +21,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
 
 import java.io.File;
 import java.io.IOException;
